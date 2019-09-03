3e39faba1de92c2bce7b3b19a305a87c907a5adf
Christian Antila <tagged-github.com@chille.se>
2019-09-04 00:28:47 +0200

    Changed awesome.quit to awesome.quit()

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -105,7 +105,7 @@ myawesomemenu = {
 	{ "manual", terminal .. " -e man awesome" },
 	{ "edit config", editor_cmd .. " " .. awesome.conffile },
 	{ "restart", awesome.restart },
-	{ "quit", awesome.quit }
+	{ "quit", function() awesome.quit() end }
 }
 
 chillemenu = {
