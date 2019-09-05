418b876450fd24d40338dd1de2e69aeae5a6ea32
Christian Antila <tagged-github.com@chille.se>
2019-09-06 00:22:23 +0200

    Moved awesome rules from rc.lua to rules.lua and cleaned up some formatting

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -264,36 +264,7 @@ clientbuttons = awful.util.table.join(
 
 -- }}}
 
--- {{{ Rules
--- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-		-- All clients will match this rule.
-		{ rule = { },
-			properties = { border_width = beautiful.border_width,
-										 border_color = beautiful.border_normal,
-										 focus = awful.client.focus.filter,
-										 raise = true,
-										 keys = clientkeys,
-										 buttons = clientbuttons } },
-		{ rule = { class = "MPlayer" },
-			properties = { floating = true } },
-		{ rule = { class = "pinentry" },
-			properties = { floating = true } },
-		{ rule = { class = "gimp" },
-			properties = { floating = true } },
-		-- Set Firefox to always map on tags number 2 of screen 1.
-		-- { rule = { class = "Firefox" },
-		--	 properties = { tag = tags[1][2] } },
-
-		-- Prevent applications from running maximized
-		{ rule = { class = "Firefox" },
-			properties = { opacity = 1, maximized = false, floating = false } },
-		{ rule = { class = "VirtualBox" },
-			properties = { opacity = 1, maximized = false, floating = false } },
-		{ rule = { class = "Inkscape" },
-			properties = { opacity = 1, maximized = false, floating = false } },
-}
--- }}}
+require("rules");
 
 -- Prevent applications from running fullscreen, they should be within their tile
 local no_fullscreen = true
