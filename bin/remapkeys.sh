9ff266cdb77bdfdba3423b868be5ee42cf1fddf2
Christian Antila <tagged-github.com@chille.se>
2019-09-07 04:48:38 +0200

    Added the magic Ctrl/Cmd remapper also for keyboard layouts with mixed BKSL / TLDE

--- bin/remapkeys.sh
+++ bin/remapkeys.sh
@@ -3,12 +3,26 @@
 if [ "$1" = "roxterm" ]; then
 	# Restore keymapping to normal when using the terminal
 	xdotool keyup Ctrl
-	setxkbmap -rules evdev -model evdev -layout chille -variant mixed_roxterm
+
+	if [ `hostname` = "chille-desktop" ]; then
+		setxkbmap -rules evdev -model evdev -layout chille -variant roxterm
+		echo "a"
+	else
+		setxkbmap -rules evdev -model evdev -layout chille -variant mixed_roxterm
+		echo "b"
+	fi
 #	xdotool keydown Super
 else
 	# Remap Control_L and Super_L to get more OS X like keybindings in all applications
 	xdotool keyup Super
-	setxkbmap -rules evdev -model evdev -layout chille -variant mixed
+
+	if [ `hostname` = "chille-desktop" ]; then
+		setxkbmap -rules evdev -model evdev -layout chille -variant basic
+		echo "c"
+	else
+		setxkbmap -rules evdev -model evdev -layout chille -variant mixed
+		echo "d"
+	fi
 #	sleep 0.2
 #	xdotool keydown Control_L
 fi
