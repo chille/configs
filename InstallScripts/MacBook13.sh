dcd084b04780aa73102eecfd30f6f2c6d2f27eab
Christian Antila <tagged-github.com@chille.se>
2019-11-30 00:20:15 +0100

    Use sudo on MacBook install scripts

--- InstallScripts/MacBook13.sh
+++ InstallScripts/MacBook13.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Make the keyboard backlight work on MacBook's
-echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="apple_backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"\nACTION=="add", SUBSYSTEM=="backlight", KERNEL=="apple_backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"\n\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chgrp video /sys/class/leds/%k/brightness"\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chmod g+w /sys/class/leds/%k/brightness"\n' > /etc/udev/rules.d/backlight.rules
-addgroup chille video
+echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="apple_backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"\nACTION=="add", SUBSYSTEM=="backlight", KERNEL=="apple_backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"\n\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chgrp video /sys/class/leds/%k/brightness"\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chmod g+w /sys/class/leds/%k/brightness"\n' | sudo tee /etc/udev/rules.d/backlight.rules
+sudo addgroup chille video
--- InstallScripts/MacBook15.sh
+++ InstallScripts/MacBook15.sh
@@ -4,5 +4,5 @@
 sudo ln -s /home/chille/configs/Xresources /home/chille/.Xresources
 
 # Make the keyboard backlight work on MacBook's
-echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="gmux_backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"\nACTION=="add", SUBSYSTEM=="backlight", KERNEL=="gmux_backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"\n\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chgrp video /sys/class/leds/%k/brightness"\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chmod g+w /sys/class/leds/%k/brightness"\n' > /etc/udev/rules.d/backlight.rules
-addgroup chille video
+echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="gmux_backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"\nACTION=="add", SUBSYSTEM=="backlight", KERNEL=="gmux_backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"\n\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chgrp video /sys/class/leds/%k/brightness"\nACTION=="add", SUBSYSTEM=="leds", KERNEL=="smc::kbd_backlight", RUN+="/bin/chmod g+w /sys/class/leds/%k/brightness"\n' | sudo tee /etc/udev/rules.d/backlight.rules
+sudo addgroup chille video
