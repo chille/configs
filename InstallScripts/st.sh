f92b188d216a9b07d9bd7e818e45dd04058584c6
Christian Antila <tagged-github.com@chille.se>
2021-03-14 22:13:37 +0100

    st: Updated install script to latest version

--- InstallScripts/st.sh
+++ InstallScripts/st.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 sudo apt-get install --no-install-recommends -y libxft-dev
-cd /home/chille/configs/
+
+cd ~/configs/
+
+# Clone git repository
 git clone https://git.suckless.org/st
 cd st/
+
+# Download and apply patches
 wget https://st.suckless.org/patches/anysize/st-anysize-0.8.1.diff
-wget https://st.suckless.org/patches/hidecursor/st-hidecursor-0.8.1.diff
+wget https://st.suckless.org/patches/hidecursor/st-hidecursor-0.8.3.diff
 wget https://st.suckless.org/patches/defaultfontsize/st-defaultfontsize-20210225-4ef0cbd.diff
 patch < st-anysize-0.8.1.diff
-patch < st-hidecursor-0.8.1.diff
-ln -s /home/chille/configs/st_config.h config.h
+patch < st-hidecursor-0.8.3.diff
 patch < st-defaultfontsize-20210225-4ef0cbd.diff
+
+ln -s ~/configs/st_config.h config.h
 make
