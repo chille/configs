2f5798f66e728e84eab15967945d01659e764a6a
Christian Antila <tagged-github.com@chille.se>
2021-03-14 22:08:49 +0100

    st: Added patch defaultfontsize

--- InstallScripts/st.sh
+++ InstallScripts/st.sh
@@ -6,7 +6,9 @@ git clone https://git.suckless.org/st
 cd st/
 wget https://st.suckless.org/patches/anysize/st-anysize-0.8.1.diff
 wget https://st.suckless.org/patches/hidecursor/st-hidecursor-0.8.1.diff
+wget https://st.suckless.org/patches/defaultfontsize/st-defaultfontsize-20210225-4ef0cbd.diff
 patch < st-anysize-0.8.1.diff
 patch < st-hidecursor-0.8.1.diff
 ln -s /home/chille/configs/st_config.h config.h
+patch < st-defaultfontsize-20210225-4ef0cbd.diff
 make
