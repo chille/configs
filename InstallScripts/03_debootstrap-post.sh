3516c4b79fe9b68f32e2012211905d08111ee289
Christian Antila <tagged-github.com@chille.se>
2021-07-16 16:56:24 +0200

    Replaced standard initramfs with tiny-initramfs

--- InstallScripts/03_debootstrap-post.sh
+++ InstallScripts/03_debootstrap-post.sh
@@ -13,9 +13,11 @@ echo -e "UUID=`blkid -s UUID -o value $DEVICE`\t/\text4\terrors=remount-ro\t0\t1
 apt install --yes --no-install-recommends linux-image-generic
 
 # Create initrd
-apt install --yes --no-install-recommends initramfs-tools
-KERNEL=`ls /usr/lib/modules/ | cut -d/ -f1 | sed 's/linux-image-//'`
-update-initramfs -u -k $KERNEL
+#apt install --yes --no-install-recommends initramfs-tools
+#KERNEL=`ls /usr/lib/modules/ | cut -d/ -f1 | sed 's/linux-image-//'`
+#update-initramfs -u -k $KERNEL
+
+apt install --yes --no-install-recommends tiny-initramfs
 
 # Create user and set password
 useradd --groups sudo --create-home $USERNAME
